46e45cf3eec7446ccbec6c22914527e5e8d2cb8e
CS434SAGroup5 <68935490+CS434SAGroup5@users.noreply.github.com>
2021-05-17 15:02:10 +0700

    create entities for domain

---
+++ WebCoreApp.Data/Entities/ProductTag.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebCoreApp.Infrastucture.SharedKernel;
+
+namespace WebCoreApp.Data.Entities
+{
+    public class ProductTag : DomainEntity<int>
+    {
+        public int ProductId { get; set; }
+
+        [StringLength(50)]
+        [Column(TypeName ="varchar(50)")]
+        public string TagId { set; get; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+
+        [ForeignKey("TagId")]
+        public virtual Tag Tag { set; get; }
+    }
+}
