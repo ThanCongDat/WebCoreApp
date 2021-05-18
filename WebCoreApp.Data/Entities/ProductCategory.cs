6ecacef75b34e518ed446313ce0e179dbe4f0f81
CS434SAGroup5 <68935490+CS434SAGroup5@users.noreply.github.com>
2021-05-18 16:25:03 +0700

    Service layer

--- WebCoreApp.Data/Entities/ProductCategory.cs
+++ WebCoreApp.Data/Entities/ProductCategory.cs
@@ -14,11 +14,29 @@ namespace WebCoreApp.Data.Entities
         {
             Products = new List<Product>();
         }
+
+        public ProductCategory(string name,string description,int? parentId, int? homeOrder, string image, bool? homeFlag,
+            int sortOrder, Status status, string seoPageTitle, string seoAlias, string seoKeywords, string seoDescription)
+        {
+            Name = name;
+            Description = description;
+            ParentId = parentId;
+            HomeOrder = homeOrder;
+            Image = image;
+            HomeFlag = homeFlag;
+            SortOrder = sortOrder;
+            Status = status;
+            SeoPageTitle = seoPageTitle;
+            SeoAlias = seoAlias;
+            SeoKeywords = seoKeywords;
+            SeoDescription = seoDescription;
+        }
         public string Name { get; set; }
         public string Description { get; set; }
         public int? ParentId { get; set; }
         public string Image { get; set; }
         public bool? HomeFlag { get; set; }
+        public int? HomeOrder { get; set; }
 
         public DateTime DateCreated { set; get; }
         public DateTime DateModified { set; get; }
